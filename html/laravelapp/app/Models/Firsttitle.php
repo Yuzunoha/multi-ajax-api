6573f96f409afcaf1c1e9dc691e90522e5c3f899
Yuzunoha <studysteadystandby@gmail.com>
2022-03-01 22:21:57 +0900

    作っただけ

---
+++ html/laravelapp/app/Models/Firsttitle.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Firsttitle extends Model
+{
+    use HasFactory;
+}
