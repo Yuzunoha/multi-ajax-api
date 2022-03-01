bd74926230e5df356f29cc297833b501a0ff943b
Yuzunoha <studysteadystandby@gmail.com>
2022-03-01 22:30:09 +0900

    入った

--- html/laravelapp/database/seeders/FulltitleSeeder.php
+++ html/laravelapp/database/seeders/FulltitleSeeder.php
@@ -3,16 +3,25 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class FulltitleSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        //
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    $texts[] = 'a';
+    $texts[] = 'b';
+    $texts[] = 'c';
+    foreach ($texts as $text) {
+      $array[] = [
+        'text' => $text,
+      ];
     }
+    DB::table('fulltitles')->insert($array);
+  }
 }
