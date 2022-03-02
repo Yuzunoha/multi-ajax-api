c8f595e9a11900991b43c80c2987ed9f326d46e6
Yuzunoha <studysteadystandby@gmail.com>
2022-03-02 15:49:55 +0900

    不要なエンドポイントの掃除

--- html/laravelapp/routes/web.php
+++ html/laravelapp/routes/web.php
@@ -14,32 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/login', 'UserController@login')->name('login');
-Route::post('/register', 'UserController@register');
-Route::group(['middleware' => ['auth:sanctum', 'RequestFilter']], function () {
-  Route::post('/logout', 'UserController@logout');
-  Route::get('/users/{id}', 'UserController@selectById');
-  Route::get('/users', 'UserController@select');
-  Route::delete('/users', 'UserController@deleteLoginUser');
-  Route::patch('/users', 'UserController@updateUser');
-  Route::post('/threads', 'ThreadController@create');
-  Route::get('/threads', 'ThreadController@select');
-  Route::get('/threads/{id}', 'ThreadController@selectById');
-  Route::patch('/threads/{id}', 'ThreadController@updateOwnThread');
-  Route::get('/replies', 'ReplyController@selectByThreadId');
-  Route::get('/replies/{id}', 'ReplyController@selectById');
-  Route::post('/replies', 'ReplyController@create');
-  Route::delete('/replies/{id}', 'ReplyController@deleteOwnReply');
-  Route::patch('/replies/{id}', 'ReplyController@updateOwnReply');
-});
-
-Route::group(['middleware' => ['RequestFilter']], function () {
-  Route::get('/test', function (Request $req) {
-    return $req;
-  });
-});
-
-
 Route::get('/delimiter', 'TitleController@delimiter');
 Route::get('/maxnum', 'TitleController@maxnum');
 Route::get('/first/{id}', 'TitleController@first');
